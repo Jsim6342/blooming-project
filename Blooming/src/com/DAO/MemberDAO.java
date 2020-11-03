be130ade0c558ffc120d73a6888d72180f11895c
정상훈 <@DESKTOP-R0N7Q4R>
2020-11-03 16:25:05 +0900

    최종 DAO/DTO + 회원가입 페이지 수정

--- Blooming/src/com/DAO/MemberDAO.java
+++ Blooming/src/com/DAO/MemberDAO.java
@@ -1,42 +1,98 @@
 package com.DAO;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+import com.DTO.MemberDTO;
+
 public class MemberDAO {
-	private String email;
-	private String pw;
-	private String tel;
-	private String nickname;
-	public MemberDAO(String email, String pw, String tel, String nickname) {
-		super();
-		this.email = email;
-		this.pw = pw;
-		this.tel = tel;
-		this.nickname = nickname;
+	private Connection conn;
+	private PreparedStatement pst;
+	private ResultSet rs;
+	//DB연결 기능
+	public void connection() {
+		try {
+			Class.forName("oracle.jdbc.driver.OracleDriver");
+			String url = "jdbc:oracle:thin:@localhost:1521:xe";
+			String user = "hr";
+			String password = "hr";
+			
+			
+			conn = DriverManager.getConnection(url, user, password);
+			
+		} catch (ClassNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		
 	}
-	public String getEmail() {
-		return email;
-	}
-	public void setEmail(String email) {
-		this.email = email;
-	}
-	public String getPw() {
-		return pw;
-	}
-	public void setPw(String pw) {
-		this.pw = pw;
-	}
-	public String getTel() {
-		return tel;
-	}
-	public void setTel(String tel) {
-		this.tel = tel;
-	}
-	public String getNickname() {
-		return nickname;
-	}
-	public void setNickname(String nickname) {
-		this.nickname = nickname;
-	}
-
 	
-
+	//DB연결 종료
+	public void close() {
+		try {
+			if(pst != null) {pst.close();}
+			if(conn != null) {conn.close();}
+			if(rs != null) {rs.close();}
+			
+		} catch(SQLException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public int member_join(MemberDTO member) {
+		int cnt = 0;
+		connection();
+		
+		
+		try {
+			String sql = "insert into member values(?,?,?,?)";
+			pst = conn.prepareStatement(sql);
+			pst.setString(1, member.getEmail());
+			pst.setString(2, member.getPw());
+			pst.setString(3, member.getTel());
+			pst.setString(4, member.getNickname());
+			cnt = pst.executeUpdate();
+			
+			
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} finally {
+			close();
+		}
+		return cnt;
+	}
+	public MemberDTO member_login(String email, String pw) {
+		MemberDTO member = null;
+		connection();
+		
+		
+		try {
+			String sql = "select * from member Where email = ? and pw = ?";
+			pst = conn.prepareStatement(sql);
+			pst.setString(1, email);
+			pst.setString(2, pw);
+			
+			rs = pst.executeQuery();
+			
+			if(rs.next()) {
+				member = new MemberDTO(rs.getString(1), rs.getString(2), rs.getString(3), rs.getString(4));
+				
+			}
+			
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} finally {
+			close();
+		}
+		return member;
+	}
 }
