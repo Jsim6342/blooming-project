c80999ccd7140da05cc0b3b52ae5b821c51ffd55
DESKTOP-GDQJFFS\82105 <ghja4009@gmail.com>
2020-11-20 09:36:40 +0900

    diagnosis_json.html 수정 survey.js 수정

--- Blooming/WebContent/survey.js
+++ Blooming/WebContent/survey.js
@@ -7,33 +7,48 @@ var json = {
     title: "우울증 진단",
     pages: [
         {
-            title: "다음 문항을 읽고 각 번호의 여러 항목 중에서 요즈음 자신에게 가장 적합하다고 생각되는 번호를 하나씩만 고르세요.",
+            title: "다음을 읽고 요즈음 자신에게 가장 적합하다고 생각되는 보기를 골라주세요.",
             questions: [
                 {
                     type: "checkbox",
-                    name: "1",
-                    title: "번",
+                    name: "",
+                    title: "",
+                    hasOther: false,
                     isRequired: true,
-                    choices: ["나는 슬프지 않다.", "나는 슬프다.", 
-							"나는 항상 슬프고 기운을 낼 수 없다.", "나는 너무나 슬프고 불행해서 도저히 견딜 수 없다."]
+                    choices: ["나는 슬프지 않다", "나는 슬프다", "나는 항상 슬프고 기운을 낼 수 없다.","나는 너무나 슬프고 불행해서 도저히 견딜 수 없다."]
                 }
             ]
-        },{
-			title: "다음 문항을 읽고 각 번호의 여러 항목 중에서 요즈음 자신에게 가장 적합하다고 생각되는 번호를 하나씩만 고르세요.",
+        }, {
+            title: "",
             questions: [
                 {
                     type: "checkbox",
-                    name: "2",
-                    title: "번",
+                    name: "",
+                    title: "",
+					hasOther: false,
                     isRequired: true,
-                    choices: ["나는 앞날에 대해서 별로 낙심하지 않는다.", "나는 앞날에 대해서 용기가 나지 않는다.", 
-							"나는 앞날에 대해 기대할 것이 아무것도 없다고 느낀다.", "나는 앞날은 아주 절망적이고 나아질 가망이 없다고 느낀다."]
+                    choices: ["나는 앞날에 대해서 별로 낙심하지 않는다.","나는 앞날에 대해서 용기가 나지 않는다.",
+							"나는 앞날에 대해 기대할 것이 아무것도 없다고 느낀다.", "나는 앞날은 아주 절망적이고 나아질 가망이 없다고 느낀다."
+                        
+                    ]
+                }
+            ]
+        }, {
+            title: "Please enter your name and e-mail",
+            questions: [
+                {
+                    type: "text",
+                    name: "name",
+                    title: "Name:"
+                }, {
+                    type: "text",
+                    name: "email",
+                    title: "Your e-mail"
                 }
             ]
         }
     ]
 };
-
 
 window.survey = new Survey.Model(json);
 
@@ -43,9 +58,8 @@ survey
         document
             .querySelector('#surveyResult')
             .textContent = "Result JSON:\n" + JSON.stringify(result.data, null, 3);
-		
     });
 
 survey.showProgressBar = 'bottom';
 
-$("#surveyElement").Survey({model: survey, onSendResult: surveySendResult, onGetResult: surveyGetResult});
+$("#surveyElement").Survey({model: survey});
